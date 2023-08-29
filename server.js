b57a4d545fd9cab748ccb55f87872347cacd83d9
Mahamadou <mmohmo@yahoo.com>
2023-08-28 23:50:02 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const jsxEngine = require('jsx-view-engine')
 const pokemon = require('./models/pokemon')
 
 const app = express()
 const port = 3000;
+
+app.set('view engine', 'jsx')
+app.engine('jsx', jsxEngine())
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    res.render('Index')
 })
 
 app.listen(port, () => {
---
+++ views/Index.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+
+const Index = props => {
+    const myStyle = {
+        color: '#ffffff',
+        backgroundColor: '#000000',
+    }
+    return(
+        <main style={myStyle}>
+            <h1>See All The Pokemon!!</h1>
+        </main>
+    )
+}
+
+module.exports = Index
